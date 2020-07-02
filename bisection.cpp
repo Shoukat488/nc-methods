140db6c6f4df6969498fd836cb09fdf64dc38eb1
nightmre789 <nightmre789@gmail.com>
2020-07-02 11:54:54 +0500

    initial

---
+++ bisection.cpp
@@ -0,0 +1,14 @@
+/*Solution(Root) of equations in one variable: ( 10 %)
+
+bisection
+false position
+newton rhapson
+secant
+fixed point
+
+Interpolation and Polynomial approximation: ( 10-15 %)
+
+Lagrange Polynomial
+Newton divided, forward, backward, and centered difference
+**/
+
