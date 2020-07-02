06d57b3981aaccd6f2dfdaf641e6ff8f434fba95
Shoukat Ali <shoukatali488@gmail.com>
2020-07-02 19:24:11 +0500

    add 4RK method

--- Differential Equations/4K.py
+++ Differential Equations/4K.py
@@ -7,7 +7,7 @@ def dydx(x, y):
 def rungeKutta(x0, y0, x, h):
     n = (int)((x - x0)/h)
     y = y0 
-    print("# \t\t k1 \t\t k2 \t\t k3 \t\t k4 \t\t x \t\t y")
+    # print("# \t\t k1 \t\t k2 \t\t k3 \t\t k4 \t\t x \t\t y")
     for i in range(1, n + 1):
         k1 = h * dydx(x0, y)
         k2 = h * dydx(x0 + 0.5 * h, y + 0.5 * k1)
@@ -15,10 +15,8 @@ def rungeKutta(x0, y0, x, h):
         k4 = h * dydx(x0 + h, y + k3)
         y = y + (1.0 / 6.0)*(k1 + 2 * k2 + 2 * k3 + k4)
         x0 = x0 + h 
-        print(i,"\t   ",k1,"\t   ",k2,"\t   ",k3,"\t   ",k4,"\t   ",x0,"\t   ",y)
-        print()
+        print("i= ",i," k1= ",k1," k2= ",k2," k3= ",k3," k4= ",k4," x0= ",x0," y= ",y)
     return y
-
 
 #Enter arguments
 x0 = 0
