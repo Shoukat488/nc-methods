322119ee94d566f2f69b93d4360c7e6ae70739ae
Shoukat Ali <shoukatali488@gmail.com>
2020-07-02 19:14:27 +0500

    add 4RK method

--- Differential Equations/Eulers.py
+++ Differential Equations/Eulers.py
@@ -1,35 +1,39 @@
-# Python Code to find approximation 
-# of a ordinary differential equation 
-# using euler method. 
+import math
 
-# Consider a differential equation 
-# dy / dx =(x + y + xy) 
-def func( x, y ): 
-	return (x + y + x * y) 
-	
-# Function for euler formula 
-def euler( x0, y, h, x ): 
-	temp = -0
+# Python Code to find approximation
+# of a ordinary differential equation
+# using euler method.
 
-	# Iterating till the point at which we 
-	# need approximation 
-    print("# iterations \t\t x \t\t y")
-	for(int i = 0 ; i < x ; i++) 
-		temp = y 
-		y = y + h * func(x0, y) 
-		x0 = x0 + h
-        print(i,"\t\t ",x0,"\t\t ",y)
+# Consider a differential equation
+# dy / dx =(x + y + xy)
+def func(x, y):
+    return x * math.exp(3 * x - 2 * y)
 
-	# Printing approximation 
-	print("Approximate solution at x = ", x, " is ", "%.6f"% y) 
-	
-# Driver Code 
-# Initial Values 
+
+# Function for euler formula
+def euler(x0, y, h, x):
+    temp = -0
+    # Iterating till the point at which we
+    # need approximation
+    i = -1
+    print("# \t\t x \t\t y ")
+    while x0 < x:
+        i = i + 1
+        temp = y
+        y = y + h * func(x0, y)
+        x0 = x0 + h
+        print(i, "\t\t", x0, "\t\t", y)
+
+    # Printing approximation
+    print("Approximate solution at x = ", x, " is ", "%.6f" % y)
+
+# Driver Code
+# Initial Values
+# x0 = input("Enter initial value: ")
+# x = input("Enter maximum value of x: ")
 x0 = 0
-y0 = 1
-h = 0.025
-
-# Value of x at which we need approximation 
-x = 0.1
-
-euler(x0, y0, h, x) 
+y0 = 0
+h = 0.5
+x = 1
+# Value of x at which we need approximation
+euler(x0, y0, h, x)
